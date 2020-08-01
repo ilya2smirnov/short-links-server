3f1a10b9148b4d709b0051bc2a983e03c980719f
smirnov_i <smirnov_i@protei.ru>
2020-08-01 16:22:22 +0300

    userModel: change return values

--- models/user.js
+++ models/user.js
@@ -7,10 +7,11 @@ exports.findByUser = async function(user) {
   let doc = await db.get().collection(collectionName).findOne({ user });
   if (doc) {
     console.log("Found doc:", doc);
+    return [doc, "User found"];
   } else {
     console.log("User", user, "not found");
+    throw [doc, "User not found"];
   }
-  return doc;
 }
 
 exports.verifyUser = async function(user, password) {
@@ -19,23 +20,27 @@ exports.verifyUser = async function(user, password) {
     let hashObj = crypt.genHash(doc.password, doc.salt);
     if (hashObj.hash === doc.hash) {
       console.log("User:", user, "verified");
-      return doc;
+      return [doc, "User verified"];
+    } else {
+      console.log("User:", user, "verified");
+      throw [doc, "Incorrect password"];
     }
   }
   console.log("User:", user, "isn't verified");
-  return null;
+  throw [null, "Username not found"];
 }
 
 exports.add = async function(user, password) {
   let doc = await exports.findByUser(user);
   if (doc) {
-    throw "User already exists";
+    console.log("User:", user, "already exists");
+    throw [doc, "User already exists"];
   }
   const hashObj = await crypt.genHash(password);
   const userObj = { user, ...hashObj };
   await db.get().collection(collectionName).insertOne(userObj);
   console.log("Add user:", userObj);
-  return userObj;
+  return [userObj, "User added"];
 }
 
 exports.deleteByUser = async function(user) {
@@ -44,14 +49,14 @@ exports.deleteByUser = async function(user) {
     let result = await db.get().collection(collectionName).deleteOne({user});
     if (result.result.n === 1 && result.result.ok === 1) {
       console.log("Deleted record:", doc);
-      return doc;
+      return [result.result.n, doc];
     } else {
       console.log("Internal error:", result);
-      throw "Internal error";
+      throw [result.result.n, null, "Internal error"];
     }
   } else {
     console.log("User", user, "doesn't exist");
-    throw "User '" + user + "' doesn't exist";
+    throw [result.result.n, null, "User '" + user + "' doesn't exist"];
   }
 }
 
