4b2fbdf4d4c23ce01a906a66284c75bdea06281a
smirnov_i <smirnov_i@protei.ru>
2020-08-01 21:59:20 +0300

    Add deleteByLinkId and getAllByUserId funcs in shortLinks model

--- models/shortLinks.js
+++ models/shortLinks.js
@@ -15,9 +15,28 @@ exports.add = async function (username, fullLink, shortLink) {
   }
   return db.get().collection(collectionName).insertOne(objToInsert)
     .then(doc => {
-      return ["", doc];
+      return doc;
     }).catch(err => {
       console.log("DB Internal error", err);
-      return ["DB Internal error"];
+      return "DB Internal error";
     })
+}
+
+exports.deleteByLinkId = async function (id) {
+  return db.get().collection(collectionName).findOneAndDelete({
+    _id: id
+  }).then(doc => {
+    if (doc) {
+      return doc;
+    } else {
+      throw "Link not found";
+    }
+  }, err => {
+    console.log("DB internal error:", err)
+    return "DB internal error";
+  })
+}
+
+exports.getAllByUserId = async function (userId) {
+  return db.get().collection(collectionName).find({ userId }).toArray()
 }
